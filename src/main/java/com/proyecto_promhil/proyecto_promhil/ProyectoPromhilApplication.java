2a1ab68a01b45583452da69289f7e0444d925cca
luis <luis985849114@outlook.com>
2025-10-22 22:08:11 -0600

    proyecto creado

---
+++ src/main/java/com/proyecto_promhil/proyecto_promhil/ProyectoPromhilApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto_promhil.proyecto_promhil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPromhilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPromhilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto_promhil/proyecto_promhil/ProyectoPromhilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_promhil.proyecto_promhil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPromhilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
